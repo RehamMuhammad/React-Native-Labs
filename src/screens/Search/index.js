a6759a094aa7f0e910b2b38df25b59882fa72b34
RehamMuhammad <Reham.Muhammad.helmy@gmail.com>
2021-11-08 18:55:00 +0200

    get movies dynamically using search button

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {ScrollView, View, FlatList} from 'react-native';
 import {dummy_data} from '../../common';
 import {MovieCard, MovieList, SearchBar} from '../../components';
@@ -7,18 +7,28 @@ import {Header} from '../../components';
 import {getAllMovies} from '../../services';
 import {client} from '../../services/client';
 import styles from './styles';
+import _ from 'lodash'
 
 export default function Search() {
   const [value, setValue] = useState('');
+  const [data, setData] = useState([]);
 
   // useEffect(() => {
   //   getData();
   // }, []);
 
-  const getData = async (_vaule) => {
+  // const getData = async (_vaule) => {
+  //   const res = await getAllMovies(_vaule);
+  //   console.log(res);
+  // };
+
+  const getData = useCallback( 
+    _.debounce( async (_vaule) => {
     const res = await getAllMovies(_vaule);
     console.log(res);
-  };
+    setData(res)
+
+  }, 1000), [],)
 
   const onChangeText = async (_vaule) => {
     setValue(_vaule);
@@ -30,13 +40,13 @@ export default function Search() {
     <View style={styles.container}>
       <View style={styles.topView}>
       <SearchBar
-      //  value={value}
+      value={value}
         onChange={(val) => onChangeText(val)}
         onPress={() => {}}
       />
       <Header text={true ? 'Search Results' : 'Recent Searches'} />
       </View>
-      <MovieList data={dummy_data} />
+      <MovieList data={data} />
     </View>
   );
 }
